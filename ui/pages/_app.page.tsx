908ceb9166d3da9bc4ab9e9b688b21d2716cc10c
LeKarimDerradji <karim.derradji.pro@gmail.com>
2022-11-24 16:57:22 +0100

    adding_states

--- ui/pages/_app.page.tsx
+++ ui/pages/_app.page.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import { useEffect, useState } from "react";
+import "./reactCOIServiceWorker";
 
-import './reactCOIServiceWorker';
+import ZkappWorkerClient from "./zkappWorkerClient";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { PrivateKey, PublicKey, Field } from "snarkyjs";
+
+let transactionFee = 0.1;
+
+export default function App() {
+  let [state, setState] = useState({
+    zkappWorkerClient: null as null | ZkappWorkerClient,
+    hasWallet: null as null | boolean,
+    hasBeenSetup: false,
+    accountExists: false,
+    currentNum: null as null | Field,
+    publicKey: null as null | PublicKey,
+    zkappPublicKey: null as null | PublicKey,
+    creatingTransaction: false,
+  });
+
+  return <div />;
 }
